402de394ffdaf6394583deee2c43efc9ddd29095
Joel Dollarhide <dollarhidejoel@gmail.com>
2016-07-06 23:35:47 -0400

    Added in some configuration for the grunt builds. With the exception of the plunker should allow for different apiKeys. Will still be available on the deployed code, but won't be stuck on the github repo. Also allows for custom build locations when running the grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,54 @@
 module.exports = function(grunt) {
+  var fs = require('fs');
+
   grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-clean')
+
+  var username = process.env.USER || process.env.USERNAME
+  var buildDir = ''
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     copy: {
-      target: {
+      init: {
+        files: [{
+          expand: true,
+          cwd: 'config',
+          src: ['template.config.json'],
+          dest: 'config/',
+          filter: function(filepath) {
+            return !(fs.existsSync('config/' + getUsernameConfigFile()))
+          },
+          rename: function(dest) {
+            return dest + getUsernameConfigFile()
+          }
+        }]
+      },
+      build: {
         files: [{
           expand: true,
           cwd: 'lib',
           src: ['**/*'],
-          dest: '../../public_html/bbInterview/dist'
+          dest: getBuildDir() + '/dist'
         },
         {
           cwd: '.',
           src: ['index.html'],
-          dest: '../../public_html/bbInterview/'
+          dest: getBuildDir() + '/'
+        },
+        {
+          expand: true,
+          cwd: 'config',
+          src: [getUsernameConfigFile()],
+          dest: getBuildDir() + '/dist/config/'
         }]
       }
     },
     clean: {
       target: {
         options: {force: true},
-        src: ['../../public_html/bbInterview/dist', '../../public_html/bbInterview/index.html']
+        src: [getBuildDir() + '/dist', getBuildDir() + '/index.html']
       }
     },
     jshint: {
@@ -35,5 +61,26 @@ module.exports = function(grunt) {
     },
   })
 
-  grunt.task.registerTask('build', ['clean', 'jshint:all', 'copy'])
+  grunt.task.registerTask('check_config', function() {
+    if (!fs.existsSync('config/' + getUsernameConfigFile())) {
+      console.log('Run "grunt init" first to generate a config file for your username. Then update the newly created config before continuing.')
+      return false
+    }
+    return true
+  })
+
+  grunt.task.registerTask('init', ['copy:init'])
+  grunt.task.registerTask('build', ['check_config', 'clean', 'jshint:all', 'copy:build'])
+
+  function getUsernameConfigFile() {
+    return username + '.config.json'
+  }
+
+  function getBuildDir() {
+    if (buildDir === '') {
+      buildDir = grunt.file.readJSON('config/' + getUsernameConfigFile()).build_dir
+    }
+
+    return buildDir
+  }
 }
